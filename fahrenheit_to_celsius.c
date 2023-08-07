b5f2183b5268282a3629e600d68aacf4ea37ac09
tr0b <trobinson3000600@gmail.com>
2023-08-06 19:05:58 -0600

    Will replace while loop with for loop

--- fahrenheit_to_celsius.c
+++ fahrenheit_to_celsius.c
@@ -3,22 +3,19 @@
 /* print Fahrenheit-Celsius table 
 * for fahr = 0,20, ... 300 */ 
 
+#define UPPER 300
+#define STEP 20
+
 main()
 {
    float fahr, celsius;
    int lower, upper, step;
 
-   lower = 0;
-   upper = 300;
-   step = 20;
-
-   fahr = lower;
    char title_fahr[] = "FAHR";
    char title_cels[] = "CELS";
    printf("%s %s\n", title_fahr, title_cels);
-   while (fahr <= upper) {
-      celsius = (5.0/9.0) * (fahr-32.0);
+   for (fahr = 0; fahr <= UPPER; fahr = fahr + STEP) {
+      celsius = (5.0/9.0) * (fahr-32);
       printf("%3.0f %6.1f\n", fahr, celsius);
-      fahr = fahr + step;
    }
 }
